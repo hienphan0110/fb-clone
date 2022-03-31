a651008ea50a7401a8a6a8cc4cca0d1836373371
Hien <phanhien01212@gmail.com>
2022-03-31 16:09:25 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   image: {
+//     domains: [
+//       "links.papareact.com",
+//       "platform-lookaside.fbsbx.com",
+//       "firebasestorage.googleapis.com",
+//     ],
+//   },
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: [
+      "i.pinimg.com",
+      "instagram.fsgn5-13.fna.fbcdn.net",
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
